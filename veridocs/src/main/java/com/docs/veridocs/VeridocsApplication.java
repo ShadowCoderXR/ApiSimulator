dd451b1d34b3865a69ce2a719924be5772153349
LuisGM875 <luis_1322134149@uptecamac.edu.mx>
2024-05-29 10:38:24 -0600

    Initial commit

---
+++ veridocs/src/main/java/com/docs/veridocs/VeridocsApplication.java
@@ -0,0 +1,13 @@
+package com.docs.veridocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeridocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeridocsApplication.class, args);
+	}
+
+}
---
+++ veridocs/src/test/java/com/docs/veridocs/VeridocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docs.veridocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeridocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
