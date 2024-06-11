c66a2a5ead120e22d91ed468380c48be2b6a8a19
SAUL IVAN GARCIA GARCIA <saul_1321124105@uptecamac.edu.mx>
2024-06-11 11:57:02 -0600

    feat(Controller): new api routes

--- src/main/java/com/demo/apisimulator/controller/VeriDocController.java
+++ src/main/java/com/demo/apisimulator/controller/VeriDocController.java
@@ -17,4 +17,23 @@ public class VeriDocController {
         LogUtil.logRequest("/api/gov/curp", request);
         return ApiResponseUtil.createResponse("curp.json");
     }
+
+    @PostMapping("/gov/ine")
+    public Map<String, Object> validateIne(@RequestBody String request) {
+        LogUtil.logRequest("/api/gov/ine", request);
+        return ApiResponseUtil.createResponse("ine.json");
+    }
+
+    @PostMapping("/gov/rfc")
+    public Map<String, Object> validateRFC(@RequestBody String request) {
+        LogUtil.logRequest("/api/gov/rfc", request);
+        return ApiResponseUtil.createResponse("rfc.json");
+    }
+
+    @PostMapping("/auth/token")
+    public Map<String, Object> validateLogin(@RequestBody String request) {
+        LogUtil.logRequest("/api/auth/token", request);
+        return ApiResponseUtil.createResponse("auth.json");
+    }
+
 }
